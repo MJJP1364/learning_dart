a7ecdc6196c023357ad683c8f9ecea9f3d17fa8c
MJJP1364 <m.j.j.p1364@gmail.com>
2025-03-28 10:55:29 +0330

    add comment

--- bin/oop/create class/class_with_constractor.dart
+++ bin/oop/create class/class_with_constractor.dart
@@ -6,6 +6,10 @@ class People {
   }
 }
 
+/// Create an instance of People and do nothing with it.
+///
+/// This is a minimal example of a main function that does something
+/// with a class.
 void main(List<String> args) {
   // ignore: unused_local_variable
   People pl1 = People();
